68e734f8665f81ed94d757200565653dd763ccca
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-27 13:24:02 +0100

    add reverse word and if to log in console if is palidrome

--- main.js
+++ main.js
@@ -4,5 +4,12 @@ let word = prompt("Scrivi una parola")
 
 console.log(word);
 
-console.log(word.split('').reverse().join(''));
+let reverseWord = word.split('').reverse().join('') //split divide la stringa in un array di caratteri, reverse ribaltà l'ordine dei caratteri della stringa, join riunisce i caratteri senza spaziature 
 
+console.log(reverseWord);
+
+if (word === reverseWord) {
+    console.log("è una parola palindroma");
+} else {
+    console.log("Mi dispiace non è palidroma");
+}
