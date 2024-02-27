2d4009890a4749ba4a55a0a549b82a081099ef92
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-27 16:38:30 +0100

    add if, now works

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -6,18 +6,43 @@ let userNumber = parseInt(prompt("Scegli un numero da uno a cinque"))
 
 let desktopNumber = parseInt(Math.floor(Math.random() * 5) + 1)
 
-console.log(oddEven, userNumber, desktopNumber);
+console.log(oddEven);
+
+let inputEven = "Hai inserito un numero pari"
+
+let inputOdd = "Hai inserito un numero dispari"
+
+if (userNumber % 2 === 0) {
+    console.log(inputEven);
+} else{
+    console.log(inputOdd);
+}
+
+console.log(userNumber, desktopNumber);
 
 let sum = userNumber + desktopNumber
 
+console.log(sum);
 
+let even = "il numero è pari"
+
+let odd = "Il numero è dispari"
+ 
 if (sum % 2 === 0) {
-    let even = "il numero è pari"
+   
     console.log(even);
 } else {
-    let odd = "Il numero è dispari"
+   
     console.log(odd);
 }
 
+
+if (sum % 2 === 0 && oddEven == "pari") {
+    console.log("Congratulazioni il numero è pari, hai vinto");
+} else if (sum % 2 === 1 && oddEven == "dispari"){
+    console.log("Congratulazioni hai scelto dispari e hai vinto");
+} else{
+    console.log("Ha vinto il computer, riprova pure");
+}
 
 
